bc9c631df059ca6135f8a8bc66c8853154fd6b27
1596944197 <1596944197@qq.com>
2023-03-17 11:40:39 +0800

    feat:dockerfile调整，env调整

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
-FROM node:16.15.1
-
-# 安装 ts-node 和 pnpm
-RUN npm install -g ts-node@10.8.1 pnpm
+FROM node:lts-alpine AS app
 
 # 将应用程序代码复制到容器中
 WORKDIR /app
 COPY . .
 
 # 安装依赖项
-RUN pnpm install
+RUN npm install --production --registry=https://registry.npm.taobao.org
+
+# 设置环境变量
+ENV NODE_ENV production
 
 # 启动应用程序
-CMD ["pnpm", "start"]
+CMD ["npx", "ts-node", "./index.ts"]
--- index.ts
+++ index.ts
@@ -88,7 +88,7 @@ async function onMessage(msg: Message) {
     fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
-        Authorization: process.env.OPENAI_API_KEY,
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
         "Content-Type": "application/json",
         cookie: process.env.COOKIE ?? "",
         origin: "chrome-extension://iaakpnchhognanibcahlpcplchdfmgma",
